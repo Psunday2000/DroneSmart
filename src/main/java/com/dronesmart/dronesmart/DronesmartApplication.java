69edd99008e2dffbcf35a4bddfc20213277a5494
Praise Chiedozie Sunday <66026084+Psunday2000@users.noreply.github.com>
2023-08-12 02:31:00 +0100

    Initial project setup

---
+++ src/main/java/com/dronesmart/dronesmart/DronesmartApplication.java
@@ -0,0 +1,13 @@
+package com.dronesmart.dronesmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DronesmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DronesmartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dronesmart/dronesmart/DronesmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dronesmart.dronesmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DronesmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
